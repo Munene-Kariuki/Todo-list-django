330d1d871490939da318b5499019ca5f08944635
Denis <denismunene999@gmail.com>
2023-03-13 23:59:28 +0300

    add login route view

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('login/', LogoutView.as_view(), name='login'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
-     path('create-task/', TaskCreate.as_view(), name='create-task'),
-     path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
-     path('delete-task/<int:pk>/', TaskDelete.as_view(), name='delete-task'),
+    path('create-task/', TaskCreate.as_view(), name='create-task'),
+    path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
+    path('delete-task/<int:pk>/', TaskDelete.as_view(), name='delete-task'),
 ]
