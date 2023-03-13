a15dc3a4c6b6996bd8cd7cad0f127ff1fcc9fc99
Denis <denismunene999@gmail.com>
2023-03-14 01:24:51 +0300

    Sign up new user

--- base/urls.py
+++ base/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete, CustomLoginView, SignupPage
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('signup/', SignupPage.as_view(), name='signup'),
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
     path('create-task/', TaskCreate.as_view(), name='create-task'),
--- base/views.py
+++ base/views.py
@@ -1,24 +1,43 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView, FormView
 from django.urls import reverse_lazy
 from django.contrib.auth.views import LoginView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login
 from .models import Task
 
 # Create your views here.
+
 
 class CustomLoginView(LoginView):
     template_name = 'base/login.html'
     fields = '__all__'
     redirect_authenticated_user = True
+
     def get_success_url(self):
         return reverse_lazy('tasks')
+    
+
+class SignupPage(FormView):
+    template_name = 'base/signup.html'
+    form_class = UserCreationForm
+    RedirectAuthenticatedUser = True
+    success_url = reverse_lazy('tasks')
+
+    def form_valid(self, form):
+        user = form.save() 
+        if user is not None:
+            login(self.request, user)
+        return super(SignupPage, self).form_valid(form)
+
 
 class TaskList(LoginRequiredMixin, ListView):
     model = Task
     context_object_name = 'tasks'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['tasks'] = context['tasks'].filter(user=self.request.user)
