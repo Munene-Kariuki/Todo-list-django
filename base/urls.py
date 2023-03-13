46a38226ee18d7a386b7c860b56d255e883a2353
Denis <denismunene999@gmail.com>
2023-03-13 17:20:58 +0300

    add delete functionality

--- base/urls.py
+++ base/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate
+from .views import TaskList, TaskDetail, TaskCreate, TaskUpdate, TaskDelete
 
 urlpatterns = [
     path('', TaskList.as_view(), name='tasks'),
     path('task/<int:pk>/', TaskDetail.as_view(), name='task'),
      path('create-task/', TaskCreate.as_view(), name='create-task'),
      path('update-task/<int:pk>/', TaskUpdate.as_view(), name='update-task'),
+     path('delete-task/<int:pk>/', TaskDelete.as_view(), name='delete-task'),
 ]
